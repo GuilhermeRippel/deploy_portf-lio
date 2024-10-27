201214fd441e8696109c8dd8cef91c5a3dc468c3
Guilherme <guilhermerippelmoraes@gmail.com>
2024-10-26 23:16:53 -0300

    Novo projeto adicionado (Tradutor)

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,6 +2,7 @@ import ProjectsCard from "./projectsCard";
 import projeto_gestao_despesas from '../assets/projeto_gestao_despesas.png';
 import projeto_cadastro_usuarios from '../assets/projeto_cadastro_usuarios.png'
 import projeto_previsao_tempo from '../assets/Projeto_Previsao_Tempo.png'
+import projeto_tradutor from '../assets/projeto_tradutor.png'
 import em_breve from '../assets/local_para_foto.png'
 
 function Projects() {
@@ -26,6 +27,13 @@ function Projects() {
         title="Projeto Previsão do Tempo"
         description="Este projeto é uma pequena página que consome a API do OpenWeather para trazer dados sobre qualquer lugar do mundo referente à clima e tempo, também tráz dados dos próximos 5 dias do lugar escolhido."
         projectLink="https://previsaotemporippel.netlify.app"
+      />
+
+      <ProjectsCard
+        image={projeto_tradutor}
+        title="Projeto Tradutor de Texto"
+        description="Este projeto é uma pequena página que consome a API do MyMemory para trazer a tradução em tempo real da linguagem que você escolher disponíveis."
+        projectLink="https://tradutorguilhermerippel.netlify.app"
       />
 
       <ProjectsCard
